6d517a3bc22943b8c30289c704557019be61b4f4
xeno <x_hermes@163.com>
2017-08-11 18:29:30 +0800

    test

--- app/src/main/java/me/xeno/unengtrainer/test/TestActivity.java
+++ app/src/main/java/me/xeno/unengtrainer/test/TestActivity.java
@@ -15,6 +15,7 @@ import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 import io.reactivex.subjects.ReplaySubject;
 import me.xeno.unengtrainer.R;
+import me.xeno.unengtrainer.util.Logger;
 
 /**
  * Created by xeno on 2017/8/9.
@@ -27,6 +28,7 @@ public class TestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.test);
+        test();
     }
 
     public void test() {
@@ -34,30 +36,49 @@ public class TestActivity extends AppCompatActivity {
 
             @Override
             public void subscribe(@NonNull ObservableEmitter<Integer> e) throws Exception {
+                Logger.info( "create " + Thread.currentThread().getName());
+                e.onNext(1);
+            }
+        }).map(new Function<Integer, Integer>() {
 
+            @Override
+            public Integer apply(@NonNull Integer integer) throws Exception {
+                Logger.info( "map " + Thread.currentThread().getName());
+                return 1;
             }
         })
+                .subscribeOn(Schedulers.computation())
                 .subscribeOn(Schedulers.io())
+                .map(new Function<Integer, Integer>() {
+
+                    @Override
+                    public Integer apply(@NonNull Integer integer) throws Exception {
+                        Logger.info( "map2 " + Thread.currentThread().getName());
+                        return 1;
+                    }
+                })
+
                 .observeOn(AndroidSchedulers.mainThread())
+                .observeOn(Schedulers.io())
                 .subscribe(new Observer<Integer>() {
                     @Override
                     public void onSubscribe(@NonNull Disposable d) {
-
+                        Logger.info( "onSubscribe " + Thread.currentThread().getName());
                     }
 
                     @Override
                     public void onNext(@NonNull Integer integer) {
-
+                        Logger.info( "onNext " + Thread.currentThread().getName());
                     }
 
                     @Override
                     public void onError(@NonNull Throwable e) {
-
+                        Logger.info(Thread.currentThread() + "");
                     }
 
                     @Override
                     public void onComplete() {
-
+                        Logger.info( "onComplete " + Thread.currentThread().getName());
                     }
                 });
     }
